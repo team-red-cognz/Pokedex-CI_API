f64dfa3b6ce74276479e89e18a44a74c6b2df807
Ferdinand-Oluwaseye <ronferd2012@gmail.com>
2019-07-31 15:00:18 +0100

    added package.json

--- index.js
+++ index.js
@@ -3,7 +3,31 @@ var express = require('express')
 var cors = require('cors');
 var config = require('./config');
 var app = express();
-var pokeRoutes = require("./pokemon-routes");
+// var pokeRoutes = require("./pokemon-routes");
+
+var express = require('express');
+var router = express.Router();
+var pokeschema = require('./pokemon-schema.js');
+var config = require('./config.js');
+var unirest = require('unirest');
+
+var poke_name = 'ditto'
+
+router.get('/getPokemon', function (err, res, body) {
+    
+            var req = unirest.get('https://pokeapi.co/api/v2/pokemon/' + poke_name + '/'); 
+
+            req.end(function (res) {
+                if (res.error) throw new Error(res.error);
+
+                // console.log(res.body);
+
+                var r = res.body
+                return r.name, r.id, r.abilities.forEach(element => {
+                    console.log(element.ability.name)
+                });
+            });
+});
 
 
 
@@ -11,11 +35,14 @@ app.use(cors({ origin: true }));
 
 app.use(express.json());
 
-app.use("/pokemon", pokeRoutes);
+// app.use("/pokemon", pokeRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+// app.use(app.router);
+// routes.initialize(app);
+
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
 
 mongoose.connect(
     config.app.MONGODB_URI,
--- pokemon-routes.js
+++ pokemon-routes.js
@@ -1,42 +1,57 @@
-var express = require('express');
-var router = express.Router();
-var pokeschema = require('./pokemon-schema.js');
-var config = require('./config.js');
-var unirest = require('unirest');
+// var express = require('express');
+// var router = express.Router();
+// var pokeschema = require('./pokemon-schema.js');
+// var config = require('./config.js');
+// var unirest = require('unirest');
 
-var poke_name = 'ditto'
+// var poke_name = 'ditto'
 
-router.get('/getPokemon', function (err, res, body) {
-    if (!err) {
-        //TODO: handle err
-        const pokedetails = pokeschema.pokemonModel.find(poke_name);
-        res.status(200).send(pokedetails);
-    }
-    else if (res.statusCode !== 200) {//etc
-        //TODO Do something with response
+// router.get('/getPokemon', function (err, res, body) {
+    
+//             var req = unirest.get('https://pokeapi.co/api/v2/pokemon/' + poke_name + '/'); 
 
-            var req = unirest.get('https://pokeapi.co/api/v2/pokemon/' + poke_name + '/'); 
+//             req.end(function (res) {
+//                 if (res.error) throw new Error(res.error);
 
-            req.end(function (res) {
-                if (res.error) throw new Error(res.error);
+//                 // console.log(res.body);
 
-                // console.log(res.body);
+//                 var r = res.body
+//                 return r.name, r.id, r.abilities.forEach(element => {
+//                     console.log(element.ability.name)
+//                 });
+//             });
+// });
 
-                var r = res.body
-                return r.name, r.id, r.abilities.forEach(element => {
-                    console.log(element.ability.name)
-                });
-            });
+
+
+
+// router.get('/getPokemon', function (err, res, body) {
+//     if (!err) {
+//         //TODO: handle err
+//         const pokedetails = pokeschema.pokemonModel.find(poke_name);
+//         res.status(200).send(pokedetails);
+//     }
+//     else if (res.statusCode !== 200) {//etc
+//         //TODO Do something with response
+
+//             var req = unirest.get('https://pokeapi.co/api/v2/pokemon/' + poke_name + '/'); 
+
+//             req.end(function (res) {
+//                 if (res.error) throw new Error(res.error);
+
+//                 // console.log(res.body);
+
+//                 var r = res.body
+//                 return r.name, r.id, r.abilities.forEach(element => {
+//                     console.log(element.ability.name)
+//                 });
+//             });
         
 
-    }else {
+//     }else {
         
-    }
-});
-
-
-
-
+//     }
+// });
 
 
 // router.post(
